39d6b8c688655dbd852afd1cef49fb48a574f48f
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2024-04-16 21:36:54 -0500

    adding svelte

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    let greeting = "";
+    if (currentHour < 12) {
+        greeting = "Buenos dias";
+    } else if (currentHour < 18) {
+        greeting = "buenas tardes";
+    } else {
+        greeting = "buenas noches ";
+    }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
